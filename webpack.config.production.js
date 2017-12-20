b0572746740d9adf043be3bd9848e500cbe69426
Tim Sandberg <tasandberg@gmail.com>
2017-12-20 01:03:42 -0800

    Shrink prod bundle

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
@@ -13,7 +14,13 @@ module.exports = {
       server: path.resolve(__dirname, 'server/')
     }
   },
-  devtool: 'source-maps',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
   module: {
     rules: [
       {
